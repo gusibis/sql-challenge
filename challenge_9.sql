a34418573bbb4d5d4a1c797904fd53e7524e9996
gusibis <gus1ibis@gmail.com>
2023-04-16 17:52:27 -0600

    more progress

--- challenge_9.sql
+++ challenge_9.sql
@@ -1,4 +1,216 @@
+--
+-- PostgreSQL database dump
+--
 
+-- Dumped from database version 15.2
+-- Dumped by pg_dump version 15.2
+
+-- Started on 2023-04-16 17:37:04
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 214 (class 1259 OID 17355)
+-- Name: departments; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.departments (
+    dept_no character varying(255) NOT NULL,
+    dept_name character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.departments OWNER TO postgres;
+
+--
+-- TOC entry 217 (class 1259 OID 17383)
+-- Name: dept_emp; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.dept_emp (
+    emp_no integer NOT NULL,
+    dept_no character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.dept_emp OWNER TO postgres;
+
+--
+-- TOC entry 218 (class 1259 OID 17398)
+-- Name: dept_manager; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.dept_manager (
+    dept_no character varying(255) NOT NULL,
+    emp_no integer NOT NULL
+);
+
+
+ALTER TABLE public.dept_manager OWNER TO postgres;
+
+--
+-- TOC entry 216 (class 1259 OID 17376)
+-- Name: employees; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.employees (
+    emp_no integer NOT NULL,
+    emp_title_id character varying(40) NOT NULL,
+    birth_date date NOT NULL,
+    first_name character varying(255) NOT NULL,
+    last_name character varying(255) NOT NULL,
+    sex character(1) NOT NULL,
+    hire_date date NOT NULL
+);
+
+
+ALTER TABLE public.employees OWNER TO postgres;
+
+--
+-- TOC entry 219 (class 1259 OID 17413)
+-- Name: salaries; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.salaries (
+    emp_no integer NOT NULL,
+    salary integer NOT NULL
+);
+
+
+ALTER TABLE public.salaries OWNER TO postgres;
+
+--
+-- TOC entry 215 (class 1259 OID 17362)
+-- Name: titles; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.titles (
+    title_id character varying(255) NOT NULL,
+    title character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.titles OWNER TO postgres;
+
+--
+-- TOC entry 3193 (class 2606 OID 17361)
+-- Name: departments departments_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.departments
+    ADD CONSTRAINT departments_pkey PRIMARY KEY (dept_no);
+
+
+--
+-- TOC entry 3199 (class 2606 OID 17387)
+-- Name: dept_emp dept_emp_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.dept_emp
+    ADD CONSTRAINT dept_emp_pkey PRIMARY KEY (emp_no, dept_no);
+
+
+--
+-- TOC entry 3201 (class 2606 OID 17402)
+-- Name: dept_manager dept_manager_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.dept_manager
+    ADD CONSTRAINT dept_manager_pkey PRIMARY KEY (dept_no, emp_no);
+
+
+--
+-- TOC entry 3197 (class 2606 OID 17382)
+-- Name: employees employees_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.employees
+    ADD CONSTRAINT employees_pkey PRIMARY KEY (emp_no);
+
+
+--
+-- TOC entry 3203 (class 2606 OID 17417)
+-- Name: salaries salaries_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.salaries
+    ADD CONSTRAINT salaries_pkey PRIMARY KEY (emp_no);
+
+
+--
+-- TOC entry 3195 (class 2606 OID 17368)
+-- Name: titles titles_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.titles
+    ADD CONSTRAINT titles_pkey PRIMARY KEY (title_id);
+
+
+--
+-- TOC entry 3204 (class 2606 OID 17393)
+-- Name: dept_emp dept_emp_dept_no_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.dept_emp
+    ADD CONSTRAINT dept_emp_dept_no_fkey FOREIGN KEY (dept_no) REFERENCES public.departments(dept_no);
+
+
+--
+-- TOC entry 3205 (class 2606 OID 17388)
+-- Name: dept_emp dept_emp_emp_no_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.dept_emp
+    ADD CONSTRAINT dept_emp_emp_no_fkey FOREIGN KEY (emp_no) REFERENCES public.employees(emp_no);
+
+
+--
+-- TOC entry 3206 (class 2606 OID 17403)
+-- Name: dept_manager dept_manager_dept_no_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.dept_manager
+    ADD CONSTRAINT dept_manager_dept_no_fkey FOREIGN KEY (dept_no) REFERENCES public.departments(dept_no);
+
+
+--
+-- TOC entry 3207 (class 2606 OID 17408)
+-- Name: dept_manager dept_manager_emp_no_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.dept_manager
+    ADD CONSTRAINT dept_manager_emp_no_fkey FOREIGN KEY (emp_no) REFERENCES public.employees(emp_no);
+
+
+--
+-- TOC entry 3208 (class 2606 OID 17418)
+-- Name: salaries salaries_emp_no_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.salaries
+    ADD CONSTRAINT salaries_emp_no_fkey FOREIGN KEY (emp_no) REFERENCES public.employees(emp_no);
+
+
+-- Completed on 2023-04-16 17:37:04
+
+--
+-- PostgreSQL database dump complete
+--
+----------------------- THE CONTENT BELOW WAS NOT GENEREATED BY pgAdmin, IT WAS PASTED BY GUS BUSTILLOS FROM pgAdmin
+/*
 -- drop table departments;
 -- drop table employees;
 -- drop table dept_emp;
@@ -48,23 +260,6 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
---Imported csv files 
 
---------------   Data Analysis ---------------- 
--- 1. List the employee number, last name, first name, sex, and salary of each employee.
-SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary FROM employees
-JOIN salaries ON employees.emp_no = salaries.emp_no
-ORDER BY emp_no;
-
--- 2. List the first name, last name, and hire date for the employees who were hired in 1986.
--- SELECT * FROM employees;
-SELECT * FROM employees WHERE CAST(employees.hire_date AS VARCHAR) LIKE '1986-%'ORDER BY emp_no;
-
--- 3. List the manager of each department along with their department number, department name, employee number, last name, and first name.
--- SELECT * FROM dept_manager;
-SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name FROM dept_manager
-JOIN departments ON departments.dept_no = dept_manager.dept_no JOIN employees ON dept_manager.emp_no = employees.emp_no
-ORDER BY emp_no;
-
--- 4. List the department number for each employee along with that employee’s employee number, last name, first name, and department name
-
+--Import csv files 
+*/
